ae37bec5d09252e4d89683f5a88bf1bb8bc474e7
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-05-09 12:27:46 +0300

    update

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+
 const app = express();
+
+const dbRes = [
+  { id: 1, name: 'John' },
+  { id: 2, name: 'Smith' },
+  { id: 3, name: 'Dino' },
+  { id: 4, name: 'Alex' },
+];
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -15,10 +23,11 @@ app.use((req, res, next) => {
 });
 
 app.get('/contact/:id', (req, res) => {
-  res.send(`<h1>Contact</h1> Параметр: ${req.params.id}`);
-  console.log(req.query);
+  // res.send(`<h1>Contact</h1> Параметр: ${req.params.id}`);
+
+  res.json(dbRes);
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('Example app listening on port 3000!');
 });
